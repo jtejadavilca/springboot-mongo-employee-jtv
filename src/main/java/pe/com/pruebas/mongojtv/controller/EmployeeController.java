b7efcee700d36c6c589505d18fe046db9092d3be
Jose Tejada <jtejadavilca@gmail.com>
2019-06-07 01:52:16 -0500

    Se agrega coressorigin

--- src/main/java/pe/com/pruebas/mongojtv/controller/EmployeeController.java
+++ src/main/java/pe/com/pruebas/mongojtv/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import pe.com.pruebas.mongojtv.model.Employee;
 import pe.com.pruebas.mongojtv.repository.EmployeeRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 	@Autowired
 	EmployeeRepository employeeRepository;
