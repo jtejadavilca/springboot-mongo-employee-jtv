fd22d705875b66c6dff5a96cdb54bd3f41ae73e7
Jose Tejada <jtejadavilca@gmail.com>
2019-06-08 04:18:07 -0500

    Se agrega método findByFirstNameRegex()

--- src/main/java/pe/com/pruebas/mongojtv/repository/EmployeeRepository.java
+++ src/main/java/pe/com/pruebas/mongojtv/repository/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package pe.com.pruebas.mongojtv.repository;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import pe.com.pruebas.mongojtv.model.Employee;
 public interface EmployeeRepository extends MongoRepository<Employee, String>{
-
+	
+	@Query("{ 'firstName' : {\"$regex\" : ?0 } }")
+	List<Employee> findByFirstNameRegex(String firstName);
 }
